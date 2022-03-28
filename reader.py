911010126a7fb7a05335bf5da5b4da3a308fcca4
asim-altayb <asomy0909@gmail.com>
2022-03-29 00:46:41 +0200

    YTPO

--- reader.py
+++ reader.py
@@ -2,6 +2,8 @@ import os,sqlite3,uuid,re
 
 conn = sqlite3.connect("powned")
 c = conn.cursor()
+c.execute('''PRAGMA synchronous = EXTRA''')
+c.execute('''PRAGMA journal_mode = WAL''')
 counter = 0
 for path, currentDirectory, files in os.walk("/home/asim/Downloads/Programming/Python lab/dataleak"):
     for file in files:
@@ -13,31 +15,34 @@ for path, currentDirectory, files in os.walk("/home/asim/Downloads/Programming/P
 
             print("Starting:",os.path.join(path, file))
             input_file = open(file,"r")
-            for line in input_file: 
-                split = ''
-                semi = line.find(';')
-                dots = line.find(':')
-                if(semi == -1):
-                    split = line.split(':',1)
-                elif(dots == -1):
-                    split = line.split(';',1)
-                if(semi < dots):
-                    split = line.split(';',1)
-                if(semi < dots):
-                    split = line.split(':',1)
-                #print(split)
-                if(len(split) ==2):
-                    email = split[0]
-                    password = split[1].split("\n")[0] or split[1]
+            with open(file,"r") as input_file:
+                lines = input_file.read().splitlines()
+                for line in lines: 
+                    split = ''
+                    semi = line.find(';')
+                    dots = line.find(':')
+                    if(semi == -1):
+                        split = line.split(':',1)
+                    elif(dots == -1):
+                        split = line.split(';',1)
+                    if(semi < dots):
+                        split = line.split(';',1)
+                    if(semi < dots):
+                        split = line.split(':',1)
+                    #print(split)
+                    if(len(split) ==2):
+                        email = split[0]
+                        password = split[1].split("\n")[0] or split[1]
 
-                    c.execute("INSERT INTO pwn (uuid, email, password) VALUES(?, ?, ?)", (str(uuid.uuid4()), email, password))
-                    count += 1
-                    counter += 1
-            conn.commit()
-            print("file :",os.path.join(path, file), count , " record")
-            with open('log', 'a') as the_file:
-                the_file.write(os.path.join(path, file)+ os.linesep)
-            print(counter, " record inserted in db")
+                        c.execute("INSERT INTO pwn (uuid, email, password) VALUES(?, ?, ?)", (str(uuid.uuid4()), email, password))
+                        count += 1
+                        counter += 1
+                conn.commit()
+            
+                print("file :",os.path.join(path, file), count , " record")
+                with open('log', 'a') as the_file:
+                    the_file.write(os.path.join(path, file)+ os.linesep)
+                print(counter, " record inserted in db")
             
             
 
