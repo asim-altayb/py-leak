99e753d26f8b3040479fb1edeed729144b3f9227
asim-altayb <asomy0909@gmail.com>
2022-04-11 13:36:13 +0200

    typo

--- multi_thread_mongo.py
+++ multi_thread_mongo.py
@@ -10,8 +10,6 @@ myclient = MongoClient("mongodb://localhost:27017/")
 # database
 db = myclient["powned"]
  
-# Created or Switched to collection
-# names: GeeksForGeeks
 collection = db["users"]
 
 start_t = time.time()
@@ -37,31 +35,29 @@ def inserter(pathes,P_ID):
         input_file = open(file_path,"r")
         current_batch = []
         insert_s_time = time.time()
-        with open(file_path,"r") as input_file:
-            lines = input_file.read().splitlines()
-            print("\n start file "+file_path+" =>" + str(P_ID))
-            for line in lines: 
-                split = split_line(line)
-                if(len(split) ==2):
-                    email = split[0]
-                    password = split[1].split("\n")[0] or split[1]
-                    current_batch.append({"email":email,"password":password})
-            INSERTED_ROWS += len(lines)
-        collection.insert_many(current_batch, ordered=False)
+        if(INSERTED_FILES < 272):
+            with open(file_path,"r") as input_file:
+                lines = input_file.read().splitlines()
+                print("\n start file "+file_path+" =>" + str(P_ID))
+                for line in lines: 
+                    split = split_line(line)
+                    if(len(split) ==2):
+                        email = split[0]
+                        password = split[1].split("\n")[0] or split[1]
+                        current_batch.append({"email":email,"password":password})
+                INSERTED_ROWS += len(lines)
+        if(INSERTED_FILES < 272):
+            collection.insert_many(current_batch, ordered=False)
+            print("\n inserted "+str(len(lines))+" in " + str(time.time()-insert_s_time)+" =>" + str(P_ID))
+            print("\n FILES PROGRESS "+str(INSERTED_FILES)+"/"+str(TOTAL_FILES)+" =>" + str(P_ID))
+            print("\n ROWS PROGRESS "+str(INSERTED_ROWS)+"/"+str(TOTAL_ROWS)+" =>" + str(P_ID))
         INSERTED_FILES +=1
-        print("\n inserted "+str(len(lines))+" in " + str(time.time()-insert_s_time)+" =>" + str(P_ID))
-        print("\n FILES PROGRESS "+str(INSERTED_FILES)+"/"+str(TOTAL_FILES)+" =>" + str(P_ID))
-        print("\n ROWS PROGRESS "+str(INSERTED_ROWS)+"/"+str(TOTAL_ROWS)+" =>" + str(P_ID))
         
-
-
-
-
 
 def path_splitter(producers_count):
     global TOTAL_FILES
     global TOTAL_ROWS
-    reader_path = '/home/asim/Downloads/Programming/Python lab/splitted/x'
+    reader_path = '/home/nawaf/splitted'
     pathes = []
     for path, currentDirectory, files in os.walk(reader_path):
         for file in files:
@@ -77,20 +73,7 @@ def main():
     max_producers = multiprocessing.cpu_count() - 2
     pathes = path_splitter(max_producers)
     print(max_producers, 'multiprocessing used')
-
-    # how many rows each producer should produce
-    each_producer_count = int(TOTAL_ROWS / max_producers)
     inserter(pathes,1)
-    # inserter_threads: List[threading.Thread] = [threading.Thread(
-    #     target=inserter, args=(pathes,i)) for i in range(multiprocessing.cpu_count() - 2)]
-
-
-    # for p in inserter_threads:
-    #     p.start()
-
-    # for p in inserter_threads:
-    #     p.join()
-
     print ("\n Time Taken: %.3f sec" % (time.time()-start_t))
 
 
